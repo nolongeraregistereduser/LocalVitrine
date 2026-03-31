a45b2dfe20a5458e901f62de64f588326e8721bb
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2026-03-31 19:32:53 +0100

    adding anfulars http testing provider to fix tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideRouter([])]
+      providers: [provideRouter([]), provideHttpClientTesting()]
     }).compileComponents();
   });
 
