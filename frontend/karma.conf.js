0f0fdcda11d528423a05afa1b25c7f7d442e5e6b
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2026-03-31 19:29:07 +0100

    fixing CI

---
+++ frontend/karma.conf.js
@@ -0,0 +1,39 @@
+// CI launcher disables Linux sandbox constraints on GitHub runners.
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma")
+    ],
+    client: {
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/frontend"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }]
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu"]
+      }
+    },
+    browsers: ["ChromeHeadless"],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
